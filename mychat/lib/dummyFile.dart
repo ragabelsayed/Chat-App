746f3f3d250efbc57dc9f780a0df4135e15373b5
RGB <ragabelsayed37@gmail.com>
2020-11-24 19:54:39 +0200

    just file contant things should now about during this app

---
+++ mychat/lib/dummyFile.dart
@@ -0,0 +1,6 @@
+//now more about animation
+//there is alot of helpful package chick it out and see it
+//what is navigation 2.0
+//what is null saftey
+//now more about material
+//   =================================
