d3a476b87bd62e6959ce230cbee8cb11a5137ce0
RGB <ragabelsayed37@gmail.com>
2020-11-27 19:22:45 +0200

    Rendering Stream Data with streamBuilder

--- mychat/lib/screen/chat_screen.dart
+++ mychat/lib/screen/chat_screen.dart
@@ -1,33 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: const EdgeInsets.all(8),
-          child: Text(
-            'This works!',
-          ),
-        ),
-      ),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('/chats/aoCHrWJXvxhtKBBqIix0/messages')
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final documents = snapshot.data.docs;
+            return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (ctx, index) => Container(
+                padding: const EdgeInsets.all(8),
+                child: Text(documents[index]['text']),
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () async {
-          await Firebase.initializeApp();
-          FirebaseFirestore.instance
-              .collection('/chats/aoCHrWJXvxhtKBBqIix0/messages')
-              .snapshots()
-              .listen((data) {
-            data.docs.forEach((doc) {
-              print(doc['text']);
-            });
-          });
-        },
+        onPressed: () => null,
       ),
     );
   }
