f4b2091636ba9f2c9fbdc989df5da40f73305612
RGB <ragabelsayed37@gmail.com>
2020-11-30 16:28:49 +0200

    Creating an Authentication Form

--- mychat/lib/widgets/auth/auth_form.dart
+++ mychat/lib/widgets/auth/auth_form.dart
@@ -6,6 +6,26 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  bool _isLogin = true;
+  String _userEmail = '';
+  String _userName = '';
+  String _userPassword = '';
+
+  void _trySubmit() {
+    final isVaild = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isVaild) {
+      _formKey.currentState.save();
+
+      print(_userEmail);
+      print(_userName);
+      print(_userPassword);
+      //Use those values to send our auth request ...
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -15,37 +35,77 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    key: ValueKey('email'),
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       labelText: 'Email address',
                     ),
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@')) {
+                        return 'Please enter a vaild email address.';
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      _userEmail = newValue;
+                    },
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Username',
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('userName'),
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 4) {
+                          return 'Please enter at least 4 characters';
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        _userName = newValue;
+                      },
                     ),
-                  ),
                   TextFormField(
+                    key: ValueKey('password'),
                     decoration: InputDecoration(
                       labelText: 'Password',
                     ),
                     obscureText: true,
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 7) {
+                        return 'Password must be at least 7 characters long.';
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      _userPassword = newValue;
+                    },
                   ),
                   SizedBox(
                     height: 12,
                   ),
                   RaisedButton(
-                    child: Text('Login'),
-                    onPressed: () {},
+                    child: Text(_isLogin ? 'Login' : 'Signup'),
+                    onPressed: _trySubmit,
                   ),
                   FlatButton(
                     textColor: Theme.of(context).primaryColor,
-                    child: Text('Create new account'),
-                    onPressed: () {},
+                    child: Text(
+                      _isLogin
+                          ? 'Create new account'
+                          : 'I already have an account',
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
                   ),
                 ],
               ),
