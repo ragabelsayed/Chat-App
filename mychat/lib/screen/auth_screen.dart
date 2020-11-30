d55c39568712eb1973a2624791f0495a97b15d3e
RGB <ragabelsayed37@gmail.com>
2020-11-30 22:31:41 +0200

    Add signIn & Create emial and password from Firebase

--- mychat/lib/screen/auth_screen.dart
+++ mychat/lib/screen/auth_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../widgets/auth/auth_form.dart';
 
 class AuthScreen extends StatefulWidget {
@@ -7,12 +9,28 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
   void _submitAuthForm(
     String email,
     String password,
     String username,
     bool isLogin,
-  ) {}
+  ) async {
+    UserCredential _userCredential;
+
+    if (isLogin) {
+      _userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } else {
+      _userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
